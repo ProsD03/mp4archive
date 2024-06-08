40340fe2e624bc525979682c7bc7154315e872ad
Alex Prosdocimo <alex.prosdocimo@gmail.com>
2024-06-08 10:30:48 +0200

    Implemented argparsing for standard parameters
    Implemented basic command-line argument parsing for default flags (input and output). Also created custom Parser for better error handling and basic library structure

--- mp4archive.py
+++ mp4archive.py
@@ -1,5 +1,33 @@
 import argparse
+import os
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-e', '--encode', action='store_true')
-parser.add_argument('-d', '--decode', action='store_true')
+from mp4archive_lib.core import MP4ArchiveFactory
+
+
+class CustomParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+
+parser = CustomParser()
+mainop = parser.add_mutually_exclusive_group()
+mainop.add_argument('-e', '--encode', help="path to file to encode into video")
+mainop.add_argument('-d', '--decode', help="path to video to decode into file")
+
+parser.add_argument("-o", "--output", help="desired path for the output", required=True)
+
+args = parser.parse_args()
+
+if args.encode:
+    if not os.path.isfile(args.encode):
+        sys.stderr.write(f"error: value of -e/--encode must be a valid file path (value: {args.encode})")
+        exit(1)
+elif args.decode:
+    if not os.path.isfile(args.encode):
+        sys.stderr.write(f"error: value of -d/--decode must be a valid file path (value: {args.encode})")
+        exit(1)
+else:
+    parser.print_help()
---
+++ mp4archive_lib/core.py
@@ -0,0 +1,3 @@
+class MP4ArchiveFactory:
+    def encode(self, input_file: str, output_file: str):
+        pass
