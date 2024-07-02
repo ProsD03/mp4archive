b1da67c47d05985d8d6829a689cc0113406cc3af
Alex Prosdocimo <alex.prosdocimo@gmail.com>
2024-07-02 15:36:56 +0200

    Moved to FFMPEG Lib and a more fault tolerant encoding
    Used FFMPEG Library (through skvideo) directly, as it is faster than using the OpenCV Wrapper and offers more control on the output. Data is now encoded using only the most significant half of every byte (the first 4 bits) and padding with 0s. This doubles the file size but makes decoding possibile, as all bytes now have a CRC code intrinsicly added to them.

--- mp4archive_lib/core.py
+++ mp4archive_lib/core.py
@@ -1,9 +1,10 @@
 import os
+import threading
 
-from PIL import Image
-import cv2
+import skvideo.io
 import numpy as np
 from tqdm import tqdm
+
 
 class MP4ArchiveFactory:
     __size__ = 1080
@@ -13,7 +14,7 @@ class MP4ArchiveFactory:
 
     __metadata_divisions__ = 4
     __metadata_divisions_size__ = int(__size__ / __metadata_divisions__)
-    __metadata_header__ = "E7E02A"
+    __metadata_header__ = "E070E00020A0"
 
     def __init__(self, size: int = None, divisions: int = None):
         if size:
@@ -21,23 +22,24 @@ class MP4ArchiveFactory:
         if divisions:
             self.__divisions__ = divisions
 
-    def __create_metadata__(self, filename: str) -> Image:
-        image = Image.new('RGB', (self.__size__, self.__size__))
+    def __create_metadata__(self, filename: str) -> np.array:
         pixels = []
         color_bytes = []
 
         pixels.append((int(self.__metadata_header__[0:2], 16), int(self.__metadata_header__[2:4], 16),
                        int(self.__metadata_header__[4:6], 16)))
+        pixels.append((int(self.__metadata_header__[6:8], 16), int(self.__metadata_header__[8:10], 16),
+                       int(self.__metadata_header__[10:12], 16)))
 
         file = os.path.split(filename)[1]
         name, ext = os.path.splitext(file)
         name = filename.encode('utf-8').hex()
         ext = ext.encode('utf-8').hex()
 
-        for i in range(10):
+        for i in range(20):
             if i > len(name) - 1:
                 break
-            color_bytes.append(name[i:i + 2])
+            color_bytes.append(name[i] + "0")
             if len(color_bytes) == 3:
                 pixels.append((int(color_bytes[0], 16), int(color_bytes[1], 16), int(color_bytes[2], 16)))
                 color_bytes = []
@@ -45,71 +47,82 @@ class MP4ArchiveFactory:
         pixels.append((0, 0, 0))
         color_bytes = []
 
-        for i in range(3):
+        for i in range(6):
             if i > len(ext) - 1:
                 break
-            color_bytes.append(ext[i:i + 2])
+            color_bytes.append(ext[i] + "0")
             if len(color_bytes) == 3:
                 pixels.append((int(color_bytes[0], 16), int(color_bytes[1], 16), int(color_bytes[2], 16)))
                 color_bytes = []
 
         divisions_hex = hex(self.__divisions__)
-        while len(divisions_hex) < 8:
+        while len(divisions_hex) <= 8:
             divisions_hex = divisions_hex + "0"
         divisions_hex = divisions_hex[2:-1]
 
-        pixels.append((int(divisions_hex[0:2], 16), int(divisions_hex[2:4], 16),
-                       int(divisions_hex[4:6], 16)))
+        for i in range(len(divisions_hex)):
+            color_bytes.append(divisions_hex[i] + "0")
+            if len(color_bytes) == 3:
+                pixels.append((int(color_bytes[0], 16), int(color_bytes[1], 16), int(color_bytes[2], 16)))
+                color_bytes = []
+
+        frame = [[(0, 0, 0) for i in range(self.__size__)] for i in range(self.__size__)]
 
         while len(pixels) > 0:
             for y_origin in range(self.__metadata_divisions__):
                 for x_origin in range(self.__metadata_divisions__):
+                    if len(pixels) == 0:
+                        break
+                    color = pixels.pop(0)
                     for y_offset in range(self.__metadata_divisions_size__):
                         for x_offset in range(self.__metadata_divisions_size__):
-                            color = pixels[0] if len(pixels) != 0 else (0, 0, 0)
-                            image.putpixel(((x_origin * self.__metadata_divisions_size__) + x_offset,
-                                            (y_origin * self.__metadata_divisions_size__) + y_offset), color)
-                    if len(pixels) != 0:
-                        pixels.pop(0)
-
-        return image
+                            frame[(y_origin * self.__metadata_divisions_size__) + y_offset][
+                                (x_origin * self.__metadata_divisions_size__) + x_offset] = color
+        return np.array(frame, dtype=np.uint8)
 
     def encode(self, input_file: str, output_file: str):
         pixels = []
-        images = [self.__create_metadata__(input_file)]
+        writer = skvideo.io.FFmpegWriter(output_file, outputdict={
+            '-vcodec': 'libx264rgb',
+            '-pix_fmt': 'rgb24',
+            '-colorspace': 'rgb',
+            '-preset': 'fast'
+        })
+        writer.writeFrame(self.__create_metadata__(input_file))
         with open(input_file, "rb") as f:
-            color_bytes = []
             data = f.read()
-            for i in range(len(data) - 1):
-                color_bytes.append(data[i:i + 1].hex())
+        color_bytes = []
+
+        for i in range(len(data)):
+            byte = hex(data[i]).split('x')[1]
+            if len(byte) < 2:
+                byte = byte + "0"
+            for nibble in byte:
+                color_bytes.append(nibble + "0")
                 if len(color_bytes) == 3:
                     pixels.append((int(color_bytes[0], 16), int(color_bytes[1], 16), int(color_bytes[2], 16)))
                     color_bytes = []
 
         pbar = tqdm(total=len(pixels))
-        pbar.set_description("Converting File into Frames... ")
+        pbar.set_description("Encoding... ")
+        threads = []
         while len(pixels) > 0:
-            image = Image.new('RGB', (self.__size__, self.__size__))
+            frame = [[(0, 0, 0) for i in range(self.__size__)] for i in range(self.__size__)]
             for y_origin in range(self.__divisions__):
                 for x_origin in range(self.__divisions__):
+                    if len(pixels) == 0:
+                        break
+                    color = pixels.pop(0)
+                    pbar.update(1)
                     for y_offset in range(self.__divisions_size__):
                         for x_offset in range(self.__divisions_size__):
-                            color = pixels[0] if len(pixels) != 0 else (0, 0, 0)
-                            image.putpixel(((x_origin * self.__divisions_size__) + x_offset,
-                                            (y_origin * self.__divisions_size__) + y_offset), color)
+                            frame[(y_origin * self.__divisions_size__) + y_offset][
+                                (x_origin * self.__divisions_size__) + x_offset] = color
+            threads.append(threading.Thread(target=writer.writeFrame, args=(np.array(frame, dtype=np.uint8),)))
+            threads[-1].start()
 
-                    if len(pixels) != 0:
-                        pixels.pop(0)
-                        pbar.update(1)
-            images.append(image.copy())
         pbar.close()
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video = cv2.VideoWriter(output_file, fourcc, 40, (self.__size__, self.__size__))
-        pbar = tqdm(total=len(images))
-        pbar.set_description("Encoding MP4... ")
-        for img in images:
-            video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
-            pbar.update(1)
-        video.release()
-        pbar.close()
+        for thread in threads:
+            thread.join()
+        writer.close()
