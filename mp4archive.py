925ad7d2351f0015eb29ed23196e6f8cfd035037
Alex Prosdocimo <alex.prosdocimo@gmail.com>
2024-07-03 10:16:44 +0200

    Revert "Started working on decoding operations"
    This reverts commit 52ffd752e800974b8bbec63b723c10f948a17b11.

--- mp4archive.py
+++ mp4archive.py
@@ -18,16 +18,13 @@ mainop = parser.add_mutually_exclusive_group()
 mainop.add_argument('-e', '--encode', help="path to file to encode into video")
 mainop.add_argument('-d', '--decode', help="path to video to decode into file")
 
-parser.add_argument("-o", "--output", help="desired path for the output")
+parser.add_argument("-o", "--output", help="desired path for the output", required=True)
 
 args = parser.parse_args()
 
 if args.encode:
     if not os.path.isfile(args.encode):
         sys.stderr.write(f"error: value of -e/--encode must be a valid file path (value: {args.encode})")
-        exit(1)
-    if not args.output:
-        sys.stderr.write(f"error: -o/--output must be set to a valid file path for encoding operations.")
         exit(1)
     encoder = MP4ArchiveFactory()
     encoder.encode(args.encode, args.output)
@@ -36,7 +33,6 @@ elif args.decode:
     if not os.path.isfile(args.decode):
         sys.stderr.write(f"error: value of -d/--decode must be a valid file path (value: {args.encode})")
         exit(1)
-    decoder = MP4ArchiveFactory()
-    decoder.decode(args.decode)
+    raise NotImplementedError
 else:
     parser.print_help()
