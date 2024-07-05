1a85158301a0cc85c96b92af2e7ebdad5c84a250
Alex Prosdocimo <alex.prosdocimo@gmail.com>
2024-07-05 11:41:03 +0200

    Encoding function implemented
    Encoding function has been implemented. It is more then x10 faster then the original implementation of the same function. It now uses numpy function to resize a smaller matrix, with only 1 pixel for each division, to the full video size. This reduces the number of for cycles from 4 to 1 in the encode functions.

--- mp4archive_lib/core.py
+++ mp4archive_lib/core.py
@@ -1,6 +1,12 @@
-import skvideo
+import skvideo.io
 import numpy as np
+import tqdm
 import string
+import os
+
+np.int = np.int32
+np.float = np.float64
+np.bool = np.bool_
 
 
 class MP4ArchiveFactory:
@@ -30,7 +36,6 @@ class MP4ArchiveFactory:
         frame[0, 0] = self._hex_to_rgb(self._magic)
         frame[0, 1] = (self._divisions, 0, 0)
         frame[0, 2] = self._hex_to_rgb(self._extension.encode("ascii").hex())
-
         codepoints = []
         pos = 0
         for i, char in enumerate(self._filename):
@@ -41,19 +46,55 @@ class MP4ArchiveFactory:
                 while len(codepoints) < 3:
                     codepoints.append(0)
             if len(codepoints) == 3:
-                frame[1 + pos//3, pos % 3] = codepoints
+                frame[1 + pos // 3, pos % 3] = codepoints
                 pos += 1
                 codepoints.clear()
-        return frame
+        return frame.repeat(self._size // 3, axis=0).repeat(self._size // 3, axis=1)
 
     def __init__(self, *args, **kwargs):
         assert skvideo.getFFmpegPath()
         self._size = kwargs.get('size', self._size)
         self._divisions = kwargs.get('divisions', self._divisions)
-        print(self._metadata_frame)
 
     def encode(self, *, input_path: str, output_path: str):
-        raise NotImplementedError
+        filename, ext = os.path.splitext(os.path.basename(input_path))
+        self._filename = filename
+        self._extension = ext.split(".")[-1]
 
-    def decode(self, *, input_path: str, output_path: str):
-        raise NotImplementedError
+        with open(input_path, "rb") as f:
+            data = f.read()
+
+        writer = skvideo.io.FFmpegWriter(output_path,
+                                         outputdict={
+                                             '-vcodec': 'libx264rgb',
+                                             '-pix_fmt': 'rgb24',
+                                             '-colorspace': 'rgb',
+                                         })
+        writer.writeFrame(self._metadata_frame)
+
+        tempbytes = []
+        frame = np.array([[[0, 0, 0] for i in range(self._divisions)] for i in range(self._divisions)])
+        pos = 0
+
+        progress = tqdm.tqdm(total=len(data), desc="Encoding...", unit="bytes")
+        for i, byte in enumerate(data):
+            tempbytes.append(byte)
+            progress.update(1)
+            if i == len(data) - 1 and len(tempbytes) < 3:
+                while len(tempbytes) < 3:
+                    tempbytes.append(0)
+            if len(tempbytes) == 3:
+                frame[pos // self._divisions, pos % self._divisions] = tempbytes
+                pos += 1
+                tempbytes.clear()
+            if pos // self._divisions >= self._divisions:
+                writer.writeFrame(
+                    frame.repeat(self._size // self._divisions, axis=0).repeat(self._size // self._divisions, axis=1))
+                pos = 0
+                frame = np.array([[[0, 0, 0] for i in range(self._divisions)] for i in range(self._divisions)])
+
+        writer.close()
+
+        def decode(self, *, input_path: str, output_path: str):
+            # smaller_img = bigger_img[::2, ::2]
+            raise NotImplementedError
