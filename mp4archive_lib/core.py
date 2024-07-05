2e1873fe2c0149293925d7860a62b8e99228f88e
Alex Prosdocimo <alex.prosdocimo@gmail.com>
2024-07-05 10:51:37 +0200

    Implemented basic support functions and parameters
    Implemented basic support functions and parameters for the MP4ArchiveFactory class. Specifically, implemented the metadata frame creation function/parameter, which creates a 3x3 RGB frame containing some information about the video (file name and extension, number of divisions per frame) and a magic number to throw errors if the video is not a valid mp4archive video.

--- mp4archive_lib/core.py
+++ mp4archive_lib/core.py
@@ -1,7 +1,59 @@
+import skvideo
+import numpy as np
+import string
+
+
 class MP4ArchiveFactory:
+    _magic = "454C45"
+
+    _size = 1080
+    _divisions = 15
+
+    _extension = "bin"
+    _filename = "filename"
+
+    @staticmethod
+    def _hex_to_rgb(hexcode: str):
+        if len(hexcode) != 6:
+            raise ValueError(f"Hexcode {hexcode} must be 6 digits (3 bytes).")
+        elif not all(c in string.hexdigits for c in hexcode):
+            raise ValueError(f"Hexcode {hexcode} must a valid hex number.")
+        return int(hexcode[:2], 16), int(hexcode[2:4], 16), int(hexcode[4:], 16)
+
+    @property
+    def _division_size(self):
+        return self._size // self._divisions
+
+    @property
+    def _metadata_frame(self):
+        frame = np.array([[[0, 0, 0] for i in range(3)] for i in range(3)])
+        frame[0, 0] = self._hex_to_rgb(self._magic)
+        frame[0, 1] = (self._divisions, 0, 0)
+        frame[0, 2] = self._hex_to_rgb(self._extension.encode("ascii").hex())
+
+        codepoints = []
+        pos = 0
+        for i, char in enumerate(self._filename):
+            if pos == 6:
+                break
+            codepoints.append(ord(char))
+            if i == len(self._filename) - 1 and len(codepoints) < 3:
+                while len(codepoints) < 3:
+                    codepoints.append(0)
+            if len(codepoints) == 3:
+                frame[1 + pos//3, pos % 3] = codepoints
+                pos += 1
+                codepoints.clear()
+        return frame
+
+    def __init__(self, *args, **kwargs):
+        assert skvideo.getFFmpegPath()
+        self._size = kwargs.get('size', self._size)
+        self._divisions = kwargs.get('divisions', self._divisions)
+        print(self._metadata_frame)
+
     def encode(self, *, input_path: str, output_path: str):
         raise NotImplementedError
 
     def decode(self, *, input_path: str, output_path: str):
         raise NotImplementedError
-
